e993751dc35f57678fb0233e99a89cbfc918f8bf
jduke330 <59979088+jduke330@users.noreply.github.com>
2020-01-16 14:53:13 -0600

    Added major
    Added a line to print my major

--- name.c
+++ name.c
@@ -1,10 +1,12 @@
  /* 
- Name: Jonathan
+ Simple hello style program printing my name
+ Name: Jonathan Duke
  Date: 2020/01/16
  */
  #include <stdio.h>
  
  int main(void) {
 		printf("Jonathan Duke\n");
+		printf("Actuarial Science\n");
 		return 0;
 }
